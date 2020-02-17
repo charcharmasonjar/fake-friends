96b367265a49d0145bf07ffedffd26c791f41889
charcharmasonjar <charlotte.dibb@gmail.com>
2020-02-16 17:11:35 -0800

    Require authenticated user for all actions on the controller

--- app/Http/Controllers/CastController.php
+++ app/Http/Controllers/CastController.php
@@ -9,6 +9,10 @@ use Illuminate\Http\Request;
 
 class CastController extends Controller
 {
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
     /**
      * Display a listing of the resource.
      *
