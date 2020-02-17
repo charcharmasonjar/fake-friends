95bbdd4c68004a5a4015567a28af20b8a489e53a
charcharmasonjar <charlotte.dibb@gmail.com>
2020-02-16 16:15:09 -0800

    Remove incorrect auth routes

--- routes/web.php
+++ routes/web.php
@@ -11,21 +11,11 @@
 |
 */
 
-use App\Http\Controllers\CastController;
 
 // Cast routes
 Route::get('/casts', 'CastController@index');
 Route::post('/cast', 'CastController@store');
 Route::delete('/cast/{cast}', 'CastController@destroy');
-
-// Authentication Routes...
-Route::get('auth/login', 'Auth\AuthController@getLogin');
-Route::post('auth/login', 'Auth\AuthController@postLogin');
-Route::get('auth/logout', 'Auth\AuthController@getLogout');
-
-// Registration Routes...
-Route::get('auth/register', 'Auth\AuthController@getRegister');
-Route::post('auth/register', 'Auth\AuthController@postRegister');
 
 Route::get('/', function () {
     return view('welcome');
