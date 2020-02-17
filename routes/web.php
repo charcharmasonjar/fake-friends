b9a556133ddb52373cb3f0abbae5d9a7d31bb6dd
charcharmasonjar <charlotte.dibb@gmail.com>
2020-02-16 16:06:36 -0800

    Add auth routes

--- routes/web.php
+++ routes/web.php
@@ -13,9 +13,24 @@
 
 use App\Http\Controllers\CastController;
 
-Route::resource('casts', 'CastController');
-Route::resource('user.casts', 'CastController');
+// Cast routes
+Route::get('/casts', 'CastController@index');
+Route::post('/cast', 'CastController@store');
+Route::delete('/cast/{cast}', 'CastController@destroy');
+
+// Authentication Routes...
+Route::get('auth/login', 'Auth\AuthController@getLogin');
+Route::post('auth/login', 'Auth\AuthController@postLogin');
+Route::get('auth/logout', 'Auth\AuthController@getLogout');
+
+// Registration Routes...
+Route::get('auth/register', 'Auth\AuthController@getRegister');
+Route::post('auth/register', 'Auth\AuthController@postRegister');
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
