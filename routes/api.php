6afc44d59556fe6512d8321ab9497db2fdce4aec
charcharmasonjar <charlotte.dibb@gmail.com>
2020-02-16 16:16:42 -0800

    Remove unused api route

--- routes/api.php
+++ routes/api.php
@@ -13,8 +13,6 @@ use Illuminate\Http\Request;
 |
 */
 
-Route::get('/casts', 'CastController@index');
-
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
