0a956ae929cfcae190edb0a9e58ee26140bd3740
charcharmasonjar <charlotte.dibb@gmail.com>
2020-02-16 17:40:11 -0800

    Create database seeder

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -12,5 +12,10 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // $this->call(UsersTableSeeder::class);
+        // $this->call(UsersTableSeeder::class);
+
+        factory(App\User::class,10)->create();
+        factory(App\Cast::class,12)->create();
+        factory(App\CastMember::class,5)->create();
     }
 }
