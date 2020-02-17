7f43e881cec2755884fe1791b6459bec9e3ac5c3
charcharmasonjar <charlotte.dibb@gmail.com>
2020-02-16 23:14:46 -0800

    Create cast member vue component

---
+++ resources/js/components/CastMember.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+        <div class="card" style="width: 10rem;">
+            <img :src="image" class="card-img-top" alt="name" />
+            <div class="card-body text-center">
+                <h5 class="card-title">{{ name }}</h5>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    props: ["name", "image"]
+};
+</script>
