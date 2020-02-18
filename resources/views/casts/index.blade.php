ccacb7f37b18900658e68ee25be6cc4803fbc7f8
charcharmasonjar <charlotte.dibb@gmail.com>
2020-02-17 21:32:33 -0800

    Show list of cast members

--- resources/views/casts/index.blade.php
+++ resources/views/casts/index.blade.php
@@ -3,6 +3,10 @@
 @section('content')
 <div class="container">
     <h1>Your casts</h1>
+    @foreach($casts as $key => $value)
+    <span>{{ $value->id}}</span>
+    <span>{{ $value->name}}</span>
+    @endforeach
     <a href="/cast/new">Create new cast</a>
 </div>
 @endsection
