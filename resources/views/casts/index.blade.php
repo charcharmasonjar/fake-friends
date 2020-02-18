f57854cfb1372c083b116f91bdf93cfc31e222bf
charcharmasonjar <charlotte.dibb@gmail.com>
2020-02-18 05:20:42 -0800

    Display cards of new cast members

--- resources/views/casts/index.blade.php
+++ resources/views/casts/index.blade.php
@@ -3,10 +3,16 @@
 @section('content')
 <div class="container">
     <h1>Your casts</h1>
+    <div class="row" style="justify-content: space-between">
     @foreach($casts as $key => $value)
-    <span>{{ $value->id}}</span>
-    <span>{{ $value->name}}</span>
-    @endforeach
+        <div class="card" style="width: 10rem; height: 14rem">
+            <img src="{{ $value->new_image_url}}" class="card-img-top" alt="ai generated face" style="height: 10rem; object-fit: cover" />
+            <div class="card-body text-center">
+                <h5 class="card-title">New {{ $value->name}}</h5>
+            </div>
+        </div>
+        @endforeach
+    </div>
     <a href="/cast/new">Create new cast</a>
 </div>
 @endsection
